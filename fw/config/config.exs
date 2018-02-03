1d55196b1a190b756e8ce267e35316242da39b35
Ruslan Kenzhebekov <rkenzheb@pinc.com>
2018-02-04 01:46:54 +0600

    Setup nerves with phoenix
    Experiment with ssh firmware update

---
+++ fw/config/config.exs
@@ -0,0 +1,66 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Customize the firmware. Uncomment all or parts of the following
+# to add files to the root filesystem or modify the firmware
+# archive.
+
+config :nerves, :firmware,
+  rootfs_overlay: "rootfs_overlay"
+#   fwup_conf: "config/fwup.conf"
+
+# Use shoehorn to start the main application. See the shoehorn
+# docs for separating out critical OTP applications such as those
+# involved with firmware updates.
+config :shoehorn,
+  init: [:nerves_runtime, :nerves_init_gadget],
+  app: Mix.Project.config()[:app]
+
+# Import target specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+# Uncomment to use target specific configurations
+
+# import_config "#{Mix.Project.config[:target]}.exs"
+#
+#
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(Path.join(System.user_home!, ".ssh/id_rsa.pub"))
+  ]
+
+
+config :nerves_init_gadget,
+  ifname: "wlan0",
+  address_method: :dhcp,
+  mdns_domain: "skeleton.local",
+  node_name: nil,
+  node_host: :mdns_domain
+
+config :nerves_network,
+  regulatory_domain: "US"
+
+key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
+
+config :nerves_network, :default,
+  wlan0: [
+    ssid: System.get_env("NERVES_NETWORK_SSID"),
+    psk: System.get_env("NERVES_NETWORK_PSK"),
+    key_mgmt: String.to_atom(key_mgmt)
+  ],
+  eth0: [
+    ipv4_address_method: :dhcp
+  ]
+
+config :ui, UiWeb.Endpoint,
+  http: [port: 80],
+  url: [host: "skeleton.local", port: 80],
+  secret_key_base: "Xxw8c3oSCMHoyDwIBCCmIpJ5UlFeUAMz0wAxMi9QCO37iKf+mUqAuw0DdD5rX2+H",
+  root: Path.dirname(__DIR__),
+  server: true,
+  render_errors: [accepts: ~w(html json)],
+  pubsub: [name: Ui.PubSub, adapter: Phoenix.PubSub.PG2]
+
